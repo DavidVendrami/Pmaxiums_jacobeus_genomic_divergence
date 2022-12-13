d22c04cd8d00fdade39c525742a85c6665ed80f4
David Vendrami <david.vendrami@student.unife.it>
2022-12-13 12:13:22 +0100

    Update Bioinformatics.sh

--- Scripts/Bioinformatics.sh
+++ Scripts/Bioinformatics.sh
@@ -1,3 +1,8 @@
+##########################################################
+### This files provide the commands to process the raw ###
+### reads all the way to SNP genotyping and fitlering. ###
+##########################################################
+
 # 1. Reads QC with process radtags
 /vol/animalbehaviour/davidlee/bin/stacks-2.60/process_radtags -p /prj/mar-in-gen/Pecten_M/originals -o /prj/mar-in-gen/Pecten_M/Clean -e pstI -c -q -r
 # Run with:
@@ -69,3 +74,129 @@ vcftools --vcf Pec_Biall_DP5_MissInd_MD80.recode.vcf --missing-indv --out Pec_Bi
 /vol/animalbehaviour/davidlee/bin/plink/plink --bfile Pec_Biall_DP5_MissInd_MD90_NoRep --aec --recodeA --out Pec_Biall_DP5_MissInd_MD90_NoRep
 ### 262 x (321,973 - MD80 | 129,260 - MD90)
 ### 262 = 27 Pj + 235 Pm
+
+###############################################################
+### The reviewer asked to check whether different fitlering ###
+### thresholds affect downstream results.                   ###
+###############################################################
+# 1. Let's see if there are SNPs genotyped only in P. jacobaeus (starting from a not --max-missing and --maf filtered dataset).
+
+/grp/animalbehaviour/davidlee/bin/plink/plink --vcf Pec_Biall_DP5_GoodSam.recode.vcf --out Pec_Biall_DP5_GoodSam --double-id --aec # Convert to plink
+
+R
+data<-read.table("Pec_Biall_DP5_GoodSam.fam",h=F)
+fid<-c()
+fid[1:23]<-"Pmax"
+fid[24:31]<-"Pjac"
+fid[32:118]<-"Pmax"
+fid[119:139]<-"Pjac"
+fid[140:284]<-"Pmax"
+data[,1]<-fid
+write.table(data,"Pec_Biall_DP5_GoodSam.fam",quote=F,col.names=F,row.names=F)
+
+/grp/animalbehaviour/davidlee/bin/plink/plink --bfile Pec_Biall_DP5_GoodSam --recodeA --aec --out Pec_Biall_DP5_GoodSam
+
+R
+library(data.table)
+data<-fread("Pec_Biall_DP5_GoodSam.raw",h=T)
+# Split dataset by species
+pm_ind<-which(data$FID=="Pmax")
+pmd<-data[pm_ind,]
+pjd<-data[-pm_ind,]
+pmd<-pmd[,-c(1:6)] # 255 samples
+pjd<-pjd[,-c(1:6)] # 29 samples
+
+# Total number of SNPs: 1009368
+
+# Count missing genotypes per locus for the two species
+pm_md<-apply(pmd,2, function(x) length(which(is.na(x))))
+pj_md<-apply(pjd,2, function(x) length(which(is.na(x))))
+
+# Amount of loci with only missing genotypes
+length(which(pm_md==255))
+# 5368: 0.5%
+length(which(pj_md==29))
+# 42519: 4.2%
+
+# Loci with genotypes only in Pjac:
+pmmd_ind<-which(pm_md==255)
+pj_md_u<-pj_md[pmmd_ind]
+length(which(pj_md_u!=29))
+# 2520, 0.24%
+# Of which with at least 80% genotypes:
+length(which(pj_md_u<5.8))
+# 33, 1.3% of pj_md_u, 0.003% of total SNPs and 0.07% of final SNPs
+
+# Quite useless. But list the names anyways and do the FST thing.
+
+ind<-which(pj_md_u<5.8)
+pju<-names(pj_md_u[ind])
+write.table(pju,"SNPs_uniquely_genotyped_in_Pj.txt",quote=F,col.names=F,row.names=F,sep="\t")
+
+sed -i 's/_[ACTG]//g' SNPs_uniquely_genotyped_in_Pj.txt
+
+# 2. No MAF filtering: already available
+
+# 3. MAF 1%
+vcftools --vcf Pec_Biall_DP5_MissInd_MD80_NoRep.recode.vcf --maf 0.01 --recode --out ../For_rev_comments/MAF/MAF_01
+
+# 4. HWE fitlerings. Calculate HWE stats separately for each pop and then exclude SNPs out of HWE in (i) half and (ii) all pops.
+# Subset file by pop to calculate HWE separately
+sed -n '1,20p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Mulroy_sub.txt
+sed -n '21,28p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Adriatic_sub.txt
+sed -n '29,49p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Oban_sub.txt
+sed -n '50,69p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Plymouth_sub.txt
+sed -n '70,87p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > CAM_sub.txt
+sed -n '88,107p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Toralla_sub.txt
+sed -n '108,126p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Gdl_sub.txt
+sed -n '127,146p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Shetland_sub.txt
+sed -n '147,166p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Norway1_sub.txt
+sed -n '167,187p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Norway2_sub.txt
+sed -n '188,208p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > NPA_sub.txt
+sed -n '209,226p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > PEB_sub.txt
+sed -n '227,246p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Bantry_sub.txt
+sed -n '247,262p' ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.fam | cut -d' ' -f2 > Tinduff_sub.txt
+
+for i in *_sub.txt
+do
+vcftools --vcf ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf --keep $i --recode --out ${i%_sub.txt}
+/grp/animalbehaviour/davidlee/bin/plink/plink --vcf ${i%_sub.txt}.recode.vcf --out ${i%_sub.txt} --double-id --aec # Let's do a plink file before MD filtering
+/grp/animalbehaviour/davidlee/bin/plink/plink --bfile ${i%_sub.txt} --aec --hardy --out ${i%_sub.txt}_HWE
+done
+
+ls -1 *HWE.hwe > file_list.txt
+
+R
+files<-read.table("file_list.txt",h=F)
+snps01<-c()
+snps05<-c()
+for (i in 1:length(files[,1])){
+data<-read.table(paste(files[i,1]),h=T)
+ind05<-which(data$P<0.05)
+ind01<-which(data$P<0.01)
+snps05<-c(snps05,as.character(data$SNP[ind05]))
+snps01<-c(snps01,as.character(data$SNP[ind01]))
+}
+table(table(snps05))
+#     1     2     3     4     5     6     7     8     9    10    11    12    13	14
+# 12312  3364   723   162    62    58    28    25    26    38    30    33    54	44
+
+table(table(snps01))
+#    1    2    3    4    5    6    7    8    9   10   11   12   13 
+# 6102  446   75   55   40   28   15   13   23   24   14   28   51 
+hwe05_all_ind<-names(which(table(snps05)==14))
+hwe05_half_ind<-names(which(table(snps05)>=7))
+hwe01_all_ind<-names(which(table(snps01)>=7))
+
+write.table(hwe05_all_ind,"hwe05_all.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(hwe05_half_ind,"hwe05_half.txt",quote=F,col.names=F,row.names=F,sep="\t")
+write.table(hwe01_all_ind,"hwe01_all.txt",quote=F,col.names=F,row.names=F,sep="\t")
+
+grep -f hwe05_all.txt ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf | cut -d$'\t' -f1,2 > hwe05_all.pos
+grep -f hwe05_half.txt ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf | cut -d$'\t' -f1,2 > hwe05_half.pos
+grep -f hwe01_all.txt ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf | cut -d$'\t' -f1,2 > hwe01_all.pos
+
+vcftools --vcf ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf --exclude-positions hwe05_all.pos --recode --out HWE_fitlered_files/hwe05_all
+vcftools --vcf ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf --exclude-positions hwe05_half.pos --recode --out HWE_fitlered_files/hwe05_half
+vcftools --vcf ../../ref_map_output/Pec_Biall_DP5_MissInd_MD80_NoRep_MAF05.recode.vcf --exclude-positions hwe01_all.pos --recode --out HWE_fitlered_files/hwe01_all
+
